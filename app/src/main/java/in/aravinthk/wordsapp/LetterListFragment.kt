2599fc900b2fc79efab38c9d2d7e86bfa1245ec1
aravinthk00 <aravinthk1809@gmail.com>
2023-08-05 11:03:34 +0530

    implement fragment navigation graph
    implement fragment navigation graph
    1. add dependencies
    2. create navi graph xml file

---
+++ app/src/main/java/in/aravinthk/wordsapp/LetterListFragment.kt
@@ -0,0 +1,85 @@
+package `in`.aravinthk.wordsapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import `in`.aravinthk.wordsapp.databinding.FragmentLetterListBinding
+
+class LetterListFragment : Fragment() {
+
+    private var _binding : FragmentLetterListBinding? = null;
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = binding.recyclerView
+        chooseLayout()
+    }
+
+    private fun chooseLayout() {
+
+        if(isLinearLayoutManager){
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        }
+        else{
+            recyclerView.layoutManager = GridLayoutManager(context, 4)
+        }
+        recyclerView.adapter = LetterAdapter()
+    }
+
+    private fun setIcon(menuItem: MenuItem){
+
+        if( menuItem == null)
+            return
+
+        if(isLinearLayoutManager){
+            ContextCompat.getDrawable(requireContext(),R.drawable.ic_linear_layout)
+        }
+        else{
+            ContextCompat.getDrawable(requireContext(), R.drawable.ic_grid_layout)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId){
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                chooseLayout()
+                setIcon(item)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/in/aravinthk/wordsapp/WordListFragment.kt
@@ -0,0 +1,46 @@
+package `in`.aravinthk.wordsapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import `in`.aravinthk.wordsapp.databinding.FragmentWordListBinding
+
+class WordListFragment: Fragment() {
+
+    private var _binding : FragmentWordListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = binding.recyclerView
+
+        recyclerView.adapter = WordAdapter(activity?.intent?.extras?.getString(DetailActivity.LETTER).toString(), requireContext())
+
+    }
+
+}
